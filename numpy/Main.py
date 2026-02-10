20b3c55a85bfe911b26a6702a72e3ba95357705d
Bakdaulet08 <zeksenbakdaulet@gmail.com>
2026-02-10 15:54:47 +0500

    First

---
+++ numpy/Main.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+arr = np.zeros((3,4))
+print(arr)
+
+ones = np.ones((4, 3))
+print(ones)
+
+eye = np.eye(4)
+print(eye)
+
+linspace = np.linspace(0, 10, 5)
+print(linspace)
+
+arange = np.arange(0, 10, 3)
