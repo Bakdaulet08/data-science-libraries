b6ca3b7459ea15144c3cc44beef83c5b2a2cc098
Bakdaulet08 <zeksenbakdaulet@gmail.com>
2026-02-10 16:08:32 +0500

    working with indexes

---
+++ numpy/Indexes.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+print(arr[1:4]) # indexes
+
+matrix = np.array([
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10 , 11, 12]
+])
+
+print(matrix[1: , :2])
